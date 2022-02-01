1e5f1b1af1989d377e59f1f5cf5fbc53e630f3d3
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-01-31 20:07:51 -0500

    commit tree 4b55fd9397b864894a4f305fe4badb6b556b8a04

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    //returns the number of blocks given a value
+    return Math.abs(someValue-42);
+}
+
+function distanceFromHqInFeet(someValue) {
+  return 264*distanceFromHqInBlocks(someValue);
+  // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+  // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+
+  // the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    return Math.abs(start-destination)*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    let dist = distanceTravelledInFeet(start,destination);
+    switch (true){
+        case dist>2500:
+            return 'cannot travel that far';
+        case dist>2000:
+            return 25;
+        case dist>400:
+            return (dist-400) * 0.02;
+        default:
+            return 0;
+    }
+}
